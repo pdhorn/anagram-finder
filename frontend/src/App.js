04872afd4e8b587ea647cf4dc844f102af5efb14
Peter Horn <peter@peters-mbp.lan>
2019-07-14 17:10:55 -0500

    logo updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import logo from './logo.svg';
+import logo from './peterh-indeed-1.jpg';
 import './App.css';
 
 const App = () => {
