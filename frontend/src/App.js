e84b2fc419aed0312a25f9f25102d990e4114906
Peter Horn <peter@peters-mbp.lan>
2019-07-14 16:54:06 -0500

    axios installed, first implementation (not working)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [stem, setStem] = useState('');
+  const [anagrams, setAnagrams] = useState([]);
+
+  const refreshList = () => {
+    axios
+      .get("http://localhost:5000/anagram?stem={stem}")
+      .then(res => setAnagrams(res.data))
+      .catch(err => console.log(err));
+    console.log(anagrams);
+  };
+
+  const updateStem = (event) => {
+    setStem(event.target.value);
+    refreshList();
+    
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Type in some letters to get all anagrams
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <input value={stem} onChange={updateStem}/>
+          <button>scramble</button>
+        </div>
+        {/* <Anagrams /> */}
       </header>
     </div>
   );
